1283c2c946101e990e64a64a0d34f058af8a8a24
wvanheemstra <wvanheemstra@icloud.com>
2014-05-31 10:14:09 +0100

    Updated

--- db/sp_CreateTablesAndViews.sql
+++ db/sp_CreateTablesAndViews.sql
@@ -4,6 +4,14 @@ CREATE PROCEDURE `SP_CREATE_TABLES_AND_VIEWS` (IN `ENTITY_NAME` varchar(255) CHA
 		SET @entityName = ENTITY_NAME;
 		SET @tableName = CONCAT('tbl_', @entityName);
 		SET @tablePrimaryKey = CONCAT('kp_', @entityName, 'ID');
+		-- Drop table		
+		SET @query = CONCAT('
+			DROP TABLE IF EXISTS `' , @tableName, '`
+		');
+		PREPARE stmt FROM @query;
+		EXECUTE stmt;
+		DEALLOCATE PREPARE stmt;		
+		-- Create table
 		SET @query = CONCAT('
 			CREATE TABLE IF NOT EXISTS `' , @tableName, '` (
 				`' , @tablePrimaryKey, '` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -20,3 +28,12 @@ delimiter ;
 
 -- Call this stored procedure like so, where Foo is the entity name:
 -- CALL SP_CREATE_TABLES_AND_VIEWS(Foo);
+
+
+
+      CREATE TABLE ENTITY_NAME 
+      (
+        TestID int(11) default NULL,
+        TestName varchar(100) default NULL
+      ) 
+      ENGINE=InnoDB DEFAULT CHARSET=utf8;
