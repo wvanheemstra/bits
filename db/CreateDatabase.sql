7d0b0c130b74efcb91adf412e841899544eb5234
wvanheemstra <wvanheemstra@icloud.com>
2014-06-02 00:12:41 +0100

    Updated

--- db/CreateDatabase.sql
+++ db/CreateDatabase.sql
@@ -203,8 +203,8 @@ CALL `bits`.SP_MAIN(@DATABASE_NAME, @ENTITY_NAME);
 	SET @valueFieldEntityValue = 'English';
 	SET @valueForeignKeyKindOfEntityID = 0;
 	SET @valueForeignKeyLanguageID = 1;
-	SET @valueTimeStampCreated = '0000-00-00 00:00:00';
-	SET @valueTimeStampUpdated = '0000-00-00 00:00:00';
+	SET @valueTimeStampCreated = CAST('0000-00-00 00:00:00' AS DATETIME);
+	SET @valueTimeStampUpdated = CAST('0000-00-00 00:00:00' AS DATETIME);
 	START TRANSACTION;
 		SET @query = CONCAT('
 			INSERT INTO `',@databaseName,'`.`',@tableEntityName,'` 
@@ -225,29 +225,105 @@ CALL `bits`.SP_MAIN(@DATABASE_NAME, @ENTITY_NAME);
 	COMMIT;	
 	-- Set foreign key checks to on
 	SET FOREIGN_KEY_CHECKS = 1;
+-- Create stored procedure that inserts an array of values
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `bits`.`SP_INSERT_ARRAY_OF_VALUES`;
+CREATE PROCEDURE `bits`.`SP_INSERT_ARRAY_OF_VALUES` (IN `DATABASE_NAME` varchar(255) CHARACTER SET 'utf8', IN `ENTITY_NAME` varchar(255) CHARACTER SET 'utf8', IN `VALUE_FIELD_ENTITY_KEY` varchar(255) CHARACTER SET 'utf8', IN `VALUE_FIELD_ENTITY_VALUE_ARRAY` varchar(255) CHARACTER SET 'utf8')
+	BEGIN
+		SET @entityName = @ENTITY_NAME;
+		SET @databaseName = @DATABASE_NAME;
+		SET @tableEntityName = CONCAT('tbl_', @entityName);
+		SET @fieldPrimaryKeyEntityID = CONCAT('pk_', @entityName, 'ID');
+		SET @valueFieldPrimaryKeyEntityID = 1;
+		SET @valueFieldForeignKeyParentID = @valueFieldPrimaryKeyEntityID;	
+		SET @valueFieldEntityKey = @VALUE_FIELD_ENTITY_KEY;
+		SET @valueFieldEntityValueArray = @VALUE_FIELD_ENTITY_VALUE_ARRAY;
+		SET @valueForeignKeyKindOfEntityID = 0;
+		SET @valueForeignKeyLanguageID = 1;
+		SET @valueTimeStampCreated = CAST('0000-00-00 00:00:00' AS DATETIME);
+		SET @valueTimeStampUpdated = CAST('0000-00-00 00:00:00' AS DATETIME);
+		SET NAMES utf8;
+		SET FOREIGN_KEY_CHECKS = 0;	
+		SET @iterator = 5; -- Make this actually a count of the number of items in the entity value array
+		WHILE @iterator > 0 DO
+		    -- NOTE: For some reason variables are not being recognized, 
+			-- also apparently variables within a while loop should not start with an @
+			START TRANSACTION;
+				SET @query = CONCAT('
+					INSERT INTO `',@databaseName,'`.`',@tableEntityName,'` 
+					VALUES(
+					 ',@valueFieldPrimaryKeyEntityID,',
+					 ',@valueFieldForeignKeyParentID,',
+					"',@valueFieldEntityKey,'",
+					"',@valueFieldEntityValue,'",
+					 ',@valueForeignKeyKindOfEntityID,',
+					 ',@valueForeignKeyLanguageID,',
+					"',@valueTimeStampCreated,'",
+					"',@valueTimeStampUpdated,'");
+				');
+				SELECT @query AS Message;
+				PREPARE stmt FROM @query;
+				EXECUTE stmt;
+				DEALLOCATE PREPARE stmt;
+			COMMIT;
+			SET @valueFieldPrimaryKeyEntityID = @valueFieldPrimaryKeyEntityID + 1;
+			SET @valueFieldForeignKeyParentID = @valueFieldPrimaryKeyEntityID;
+
+			SET @iterator = @iterator - 1;
+		END WHILE;
+		SET FOREIGN_KEY_CHECKS = 1;
+	END $$
+DELIMITER ;
 -- Call stored procedure main, proving it with the database name and entity name	
 SET @ENTITY_NAME = 'Entity';
 CALL `bits`.SP_MAIN(@DATABASE_NAME, @ENTITY_NAME);
-	-- Set names
-	SET NAMES utf8;
-	-- Set foreign key checks to off
-	SET FOREIGN_KEY_CHECKS = 0;	
-	SET @entityName = @ENTITY_NAME;
-	SET @databaseName = @DATABASE_NAME;
-	SET @tableEntityName = CONCAT('tbl_', @entityName);
-	SET @fieldPrimaryKeyEntityID = CONCAT('pk_', @entityName, 'ID');
-	SET @valueFieldPrimaryKeyEntityID = 1;
-	SET @valueFieldForeignKeyParentID = @valueFieldPrimaryKeyEntityID;	
-	SET @valueFieldEntityKey = 'Entity';
-	SET @valueFieldEntityValueArray = 'Entity,Language,Individual,Name,';
-	SET @valueForeignKeyKindOfEntityID = 0;
-	SET @valueForeignKeyLanguageID = 1;
-	SET @valueTimeStampCreated = '0000-00-00 00:00:00';
-	SET @valueTimeStampUpdated = '0000-00-00 00:00:00';
-	WHILE (LOCATE(',', @valueFieldEntityValueArray) > 0)
-	DO
+-- Set values for entity table, then call stored procedure insert array of values, providing it with the key and array of values
+SET @VALUE_FIELD_ENTITY_KEY = 'Entity';
+SET @VALUE_FIELD_ENTITY_VALUE_ARRAY = 'Entity|Language|Individual|Name';
+CALL `bits`.SP_INSERT_ARRAY_OF_VALUES(@DATABASE_NAME, @ENTITY_NAME, @VALUE_FIELD_ENTITY_KEY, @VALUE_FIELD_ENTITY_VALUE_ARRAY);
+
+	
+/*	
+	SET @separator = '|';
+	SET @separatorLength = CHAR_LENGTH(@separator);
+	
+	DECLARE i INT DEFAULT 1;
+	
+	WHILE (i<=2) DO
+		SET @valueFieldEntityValue = SUBSTRING_INDEX(@valueFieldEntityValueArray, @separator, 1);
+		
+		-- DO WORK
+		SELECT @valueFieldEntityValue AS Message_valueFieldEntityValue;
+		
+		SET @valueFieldEntityValueArray = SUBSTRING(@valueFieldEntityValueArray, CHAR_LENGTH(@valueFieldEntityValue) + @separatorLength + 1);
+		SET i=i+1;
+	END WHILE;
+	
+	
+	
+--	WHILE (@valueFieldEntityValueArray != '') > 0 DO
+--		SET @currentValue = SUBSTRING_INDEX(@valueFieldEntityValueArray, @separator, 1);
+	 
+		-- DO WORK
+--		SELECT @currentValue AS Message_CurrentValue;
+	 
+--		SET @valueFieldEntityValueArray = SUBSTRING(@valueFieldEntityValueArray, CHAR_LENGTH(@currentValue) + @separatorLength + 1);
+--	END WHILE;
+	
+*/	
+	
+	
+/*	
+	WHILE (LOCATE('|', @valueFieldEntityValueArray) > 0) DO
+	
+		SELECT LOCATE('|', @valueFieldEntityValueArray) AS Message_Locate;
+	
 		SET @valueFieldEntityValue = ELT(1, @valueFieldEntityValueArray);
-		SET @valueFieldEntityValue = SUBSTRING(@valueFieldEntityValueArray, LOCATE(',',@valueFieldEntityValueArray) + 1);
+		
+		SELECT @valueFieldEntityValueArray AS Message_valueFieldEntityValueArray;
+		SELECT @valueFieldEntityValue AS Message_valueFieldEntityValue;
+		
+		SET @valueFieldEntityValue = SUBSTRING(@valueFieldEntityValueArray, LOCATE('|',@valueFieldEntityValueArray) + 1);
 		START TRANSACTION;
 			SET @query = CONCAT('
 				INSERT INTO `',@databaseName,'`.`',@tableEntityName,'` 
@@ -269,5 +345,7 @@ CALL `bits`.SP_MAIN(@DATABASE_NAME, @ENTITY_NAME);
 		SET @valueFieldPrimaryKeyEntityID = @valueFieldPrimaryKeyEntityID + 1;
 		SET @valueFieldForeignKeyParentID = @valueFieldPrimaryKeyEntityID;
 	END WHILE;	
+*/	
+	
 	-- Set foreign key checks to on
-	SET FOREIGN_KEY_CHECKS = 1;
+--	SET FOREIGN_KEY_CHECKS = 1;
